bd50b23fe8add60b813a93e6ab26c5d469675348
Alan Gamboa <alangam97@gmail.com>
2021-01-03 13:58:53 -0600

    probando con don

--- app/src/main/java/com/example/nutritionapp/MainActivity.java
+++ app/src/main/java/com/example/nutritionapp/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnRegister;
     Button btnLogin;
 
+
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
