9394b99f7a5e62f62dae8cca92a1bce703b1e679
Alan Gamboa <alangam97@gmail.com>
2021-01-03 13:30:31 -0600

    intentando conectar

--- app/src/main/java/com/example/nutritionapp/MainActivity.java
+++ app/src/main/java/com/example/nutritionapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnRegister;
     Button btnLogin;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+
     }
 
 
